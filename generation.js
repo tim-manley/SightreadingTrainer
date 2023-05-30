bf3c4d704d99e3b772575ebeebea553f833f53de
Tim Manley <p14tman14tman@gmail.com>
2023-05-30 12:26:36 -0400

    first commit

---
+++ generation.js
@@ -0,0 +1,54 @@
+// Generates random ABC music encoding
+
+const numToNote = {
+    1: "A",
+    2: "B",
+    3: "C",
+    4: "D",
+    5: "E",
+    6: "F",
+    7: "G"
+};
+
+function generateHeader() {
+    let abcString = "X:1\nT:Random Bars\nM:4/4\nL:1/8\nK:C\n";
+    return abcString;
+}
+
+// Creates one random bar in C, 4/4, using string concatenation
+function generateBar(needsHeader) {
+    let abcString = "";
+    if (needsHeader) {
+        abcString = abcString + generateHeader();
+    }
+    let length = 0; // Length of notes selected
+    while (length < 8) { // While not full
+        let newNote = "";
+        let altered = Math.random();
+        if (altered < 0.3) {
+            newNote = newNote + "^";
+        } else if (altered > 0.7) {
+            newNote = newNote + "_";
+        }
+        let noteValue = Math.floor(1 + Math.random() * 7);
+        console.log(noteValue);
+        newNote = newNote + numToNote[noteValue];
+        let newNoteLength = Math.floor(1 + Math.random() * 8); // Maybe just use remaining time?
+        while (length + newNoteLength > 8 || newNoteLength == 5) { // 5 is not well defined
+            newNoteLength = Math.floor(1 + Math.random() * 8)
+        }
+        newNote = newNote + newNoteLength;
+        length = length + newNoteLength;
+        abcString = abcString + newNote + " ";
+        console.log(newNote);
+    }
+    return abcString;
+}
+
+function generateBars(numBars) {
+    let abcString = generateHeader();
+    for (let i = 0; i < numBars; i++) {
+        abcString = abcString + generateBar(false) + "| ";
+    }
+    return abcString;
+}
