6dc76edd0d3f36ee13143fa207c0b6d280daa106
Tim Manley <p14tman14tman@gmail.com>
2023-06-09 15:47:17 +0100

    Pre model change commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+var firebase = require('firebase');
+var firebaseui = require('firebaseui');
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
